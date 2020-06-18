4b94f5ccfa044d8e0d6df87431f616956bcf0904
Kaic Carvalho <kaic15_fernandes@hotmail.com>
2020-06-18 14:59:05 -0300

    Primeiro commit do projeto Health Track

---
+++ js/dashboard.js
@@ -0,0 +1,11 @@
+/* Abrir em 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /*Fechar*/
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+ 
